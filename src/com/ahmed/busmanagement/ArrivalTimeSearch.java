994beece29f28c2b9fd8cb790a0c5face56fe23b
Ahmed Mahdi <mahdiah@tcd.ie>
2022-04-04 12:58:06 +0100

    added ArrivalTimeSearch class

---
+++ src/com/ahmed/busmanagement/ArrivalTimeSearch.java
@@ -0,0 +1,13 @@
+package com.ahmed.busmanagement;
+
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+public class ArrivalTimeSearch {
+
+
+}
